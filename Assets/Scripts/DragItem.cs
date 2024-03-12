68a800c3373cec61cbc5464e1b3ed1ad6bab7121
EdmundoMarquez <lw.marquez@gmail.com>
2024-03-12 16:58:53 -0600

    feat: Create draggable items and drop slots, added key ids and prefab

---
+++ Assets/Scripts/DragItem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DragItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private ObjectId _objectId = null;
+    [SerializeField] private Canvas _canvas = null;
+    [SerializeField] private GridLayoutGroup _gridLayout = null;
+    public ObjectId Id => _objectId;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+        _gridLayout.enabled = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        _gridLayout.enabled = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+}
---
+++ Assets/Scripts/DropSlot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class DropSlot : MonoBehaviour, IDropHandler
+{
+    [SerializeField] private ObjectId _objectId = null;
+    [SerializeField] private Image _slotImage = null;
+    private RectTransform rectTransform;
+
+    private void Awake() 
+    {
+        rectTransform = GetComponent<RectTransform>();    
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag != null)
+        {
+            DragItem item = eventData.pointerDrag.GetComponent<DragItem>();
+            if(item.Id.Value == _objectId.Value)
+            {
+                _slotImage.DOFade(0f, 0.5f);
+                _slotImage.rectTransform.DOAnchorPosY(-45f, 0.8f);
+                Debug.Log("Lock Removed");
+            }
+            else
+            {
+                _slotImage.rectTransform.DOShakeAnchorPos(0.8f, new Vector3(2,0,0));
+            }
+        }
+    }
+}
