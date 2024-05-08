e1c932f4a80e862075b5e1664d6e582fb3de6ee1
Edmundo Márquez <edmundomarquez@protonmail.com>
2024-05-08 16:20:00 -0600

    chore: Imported PCG solution to project

--- Assets/Scripts/Dungeon Generator/Data/SimpleRandomWalkSO.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "SimpleRandomWalkParameters", menuName = "SimpleRandomWalkSO", order = 0)]
-public class SimpleRandomWalkSO : ScriptableObject 
-{
-    public int iterations = 10, walkLength = 10;
-    public bool startRandomlyEachIteration = true;
-}
