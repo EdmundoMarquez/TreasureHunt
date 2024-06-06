15331b4e697914c51ebd9146a0b30b1066811753
Edmundo Márquez <edmundomarquez@protonmail.com>
2024-06-06 16:12:33 -0600

    feat: Added pause view and created game over and pause screen prefabs, implemented audio settings controller

---
+++ Assets/Scripts/Common/AudioSettingsController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace Treasure.Audio
+{
+    public class AudioSettingsController : MonoBehaviour
+    {
+        [SerializeField] private AudioMixer _audioMixer = null;
+        [SerializeField] private string _volumeParameter = null;
+        [SerializeField] private Slider _slider = null;
+        [SerializeField] private float _multiplier = 30f;
+        [SerializeField] private Button _toggleButton = null;
+        [SerializeField] private Image _toggleIcon = null;
+        // [SerializeField] private GameSaveData _playerData = null;
+        private bool toggleSlider = true;
+        private void Awake()
+        {
+            if (_slider == null) return;
+            _slider.onValueChanged.AddListener(HandleSliderValueChanged);
+            _toggleButton.onClick.AddListener(ToggleSlider);
+        }
+        private void Start()
+        {
+            // if (_slider != null)
+            //     _slider.value = _playerData.PlayerPreferences.MusicVolumen;
+            // HandleSliderValueChanged(_playerData.PlayerPreferences.MusicVolumen);
+        }
+        private void HandleSliderValueChanged(float value)
+        {
+            // _playerData.PlayerPreferences.MusicVolumen = value;
+            _audioMixer.SetFloat(_volumeParameter, Mathf.Log10(value) * _multiplier);
+        }
+
+        private void ToggleSlider()
+        {
+            toggleSlider = !toggleSlider;
+            _toggleIcon.DOFade(toggleSlider ? 1f : 0.5f, 0.3f).SetUpdate(true);
+            _slider.interactable = toggleSlider;
+
+            if(toggleSlider)
+                _audioMixer.SetFloat(_volumeParameter, Mathf.Log10(_slider.value) * _multiplier);
+            else
+                _audioMixer.SetFloat(_volumeParameter, Mathf.Log10(0f) * _multiplier);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Dungeon/PauseView.cs
@@ -0,0 +1,25 @@
+﻿namespace Treasure.Dungeon
+{
+    using UnityEngine;
+    using Treasure.Common;
+    using DG.Tweening;
+    using System.Collections;
+    using TMPro;
+
+    public class PauseView : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _canvasGroup = null;
+        [SerializeField] private CanvasGroupFacade _canvasFacade = null;
+        
+
+        public void ToggleVisibility(bool toggle)
+        {
+            Time.timeScale = toggle ? 0f : 1f;
+            _canvasGroup.DOFade(toggle ? 1f: 0f, 0.3f).SetUpdate(true);
+            _canvasGroup.interactable = toggle;
+            _canvasGroup.blocksRaycasts = toggle;
+
+            _canvasFacade.ToggleVisibility(!toggle);
+        }
+    }
+}
