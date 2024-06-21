441aea1d0289924edb5f3f22c1561d40f6628c68
EdmundoMarquez <lw.marquez@gmail.com>
2024-06-20 18:52:21 -0600

    fix: Enable chest collider after all traps have been generated (avoids collision errors when removing cluttered spike traps), renamed isUnlocked variable to canInteract and disabled interaction after activating a trap

--- Assets/Scripts/Chests/Chest.cs
+++ Assets/Scripts/Chests/Chest.cs
@@ -9,21 +9,26 @@
     {
         public ChestData ChestData;
         public int CurrentTries;
+        [SerializeField] private TrapBuilder _trapBuilder = null;
         [SerializeField] private GameObject _lockedSprite;
         [SerializeField] private GameObject _unlockedSprite;
         [SerializeField] private SpriteRenderer _treasureSprite;
         [SerializeField] private MinimapIconDisplay _minimapIcon = null;
         private WordData _wordToSolve;
-        private bool _isUnlocked;
-        public bool CanInteract => !_isUnlocked;
+        private bool _canInteract;
+        public bool CanInteract => _canInteract;
         private Sequence UnlockSequence;
+        private Collider2D _collider;
 
         private void Start()
         {
+            _collider = GetComponent<Collider2D>();
             _wordToSolve = ChestData.RandomizeWord();
             CurrentTries = ChestData.Tries;
             _minimapIcon.Init(_lockedSprite.GetComponent<SpriteRenderer>().sprite);
-
+            _trapBuilder.Init(CurrentTries);
+            _collider.enabled = true;
+            
             EventBus<OnChestGenerated>.Raise(new OnChestGenerated());
         }
 
@@ -40,7 +45,7 @@
 
         public void ToggleLock(bool unlock)
         {
-            _isUnlocked = unlock;
+            _canInteract = !unlock;
             _lockedSprite.SetActive(!unlock);
             _unlockedSprite.SetActive(unlock);
 
@@ -59,18 +64,20 @@
         public void ActivateTrap()
         {
             CurrentTries = 0;
+            _trapBuilder.ActivateGeneratedTraps();
+            _canInteract = false;
         }
 
-        public void PlayFloatingTreasureAnimation(Sprite rewardIcon)
-        {
-            _treasureSprite.sprite = rewardIcon;
-            _treasureSprite.gameObject.SetActive(true);
+        // public void PlayFloatingTreasureAnimation(Sprite rewardIcon)
+        // {
+        //     _treasureSprite.sprite = rewardIcon;
+        //     _treasureSprite.gameObject.SetActive(true);
 
-            UnlockSequence = DOTween.Sequence();
+        //     UnlockSequence = DOTween.Sequence();
 
-            UnlockSequence.Append(_treasureSprite.transform.DOLocalMoveY(0.5f, 0.5f))
-            .AppendInterval(0.25f)
-            .Append(_treasureSprite.DOFade(0f, 0.2f));
-        }
+        //     UnlockSequence.Append(_treasureSprite.transform.DOLocalMoveY(0.5f, 0.5f))
+        //     .AppendInterval(0.25f)
+        //     .Append(_treasureSprite.DOFade(0f, 0.2f));
+        // }
     }
 }
