f461773346f7541936d228d70f94f538def7de39
EdmundoMarquez <lw.marquez@gmail.com>
2024-06-20 18:39:14 -0600

    feat: Created trap builder, implemented spike traps elements and arrow trap class, added trap ids

---
+++ Assets/Scripts/Chests/ArrowTrap.cs
@@ -0,0 +1,104 @@
+﻿namespace Treasure.Chests
+{
+    using UnityEngine;
+    using Treasure.Common;
+    using System;
+    using System.Collections;
+    using DG.Tweening;
+    using Random = UnityEngine.Random;
+
+    public class ArrowTrap : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 3f;
+        [SerializeField] private ContactFilter2D _contactFilter;
+        [SerializeField] private DataProperty[] _damageProperties;
+        [SerializeField] private DamageInstigator _damageInstigator = null;
+        private SpriteRenderer _arrowSprite;
+        private Vector2 _moveDirection;
+        private bool canTick;
+        
+        public void Init()
+        {
+            _arrowSprite = GetComponent<SpriteRenderer>();
+            _arrowSprite.enabled = false;
+            _damageInstigator.Init(_damageProperties);
+            _damageInstigator.onHit += OnHit;
+        }
+        public void Activate()
+        {
+            _damageInstigator.ToggleInstigator(true);
+            transform.position = GetStartPoint();
+            canTick = true;
+        }
+
+        private void Update()
+        {
+            if(!canTick) return;
+            transform.position += (Vector3)_moveDirection * _speed * Time.deltaTime;
+
+            if(_arrowSprite.enabled) return;
+            Collider2D[] results = new Collider2D[2];
+            int hits = Physics2D.OverlapCircle(transform.position, 1f, _contactFilter, results);
+
+            if(hits > 0)
+                _arrowSprite.enabled = true;
+        }
+
+        private Vector2 GetStartPoint()
+        {
+            var spriteSize = GetComponent<SpriteRenderer>().bounds.size.x * .5f; // Working with a simple box here, adapt to you necessity
+
+            var cam = Camera.main;// Camera component to get their size, if this change in runtime make sure to update values
+            var camHeight = cam.orthographicSize;
+            var camWidth = cam.orthographicSize * cam.aspect;
+
+            float yMin = -camHeight + spriteSize; // lower bound
+            float yMax = camHeight - spriteSize; // upper bound
+        
+            float xMin = -camWidth + spriteSize; // left bound
+            float xMax = camWidth - spriteSize; // right bound 
+
+            float randValue = Random.value;
+            Vector2 spawnPoint = Vector2.zero;
+            Vector2 offset = (Vector2)(Camera.main.transform.position - transform.position);
+
+            if(randValue > 0.75f) //upwards
+            {
+                transform.rotation = Quaternion.Euler(0,0,180);
+                spawnPoint = new Vector2(0 , yMax);
+                _moveDirection = Vector2.down;
+            }
+            else if (randValue > 0.5f) //downwards
+            {
+                spawnPoint = new Vector2(0 , yMin);
+                _moveDirection = Vector2.up;
+            }
+            else if(randValue > 0.25f) //left
+            {
+                transform.rotation = Quaternion.Euler(0,0,-90);
+                spawnPoint = new Vector2(xMin , 0);
+                _moveDirection = Vector2.right;
+            }
+            else //right
+            {
+                transform.rotation = Quaternion.Euler(0,0,90);
+                spawnPoint = new Vector2(xMax, 0);
+                _moveDirection = Vector2.left;
+            }
+            return spawnPoint + offset;
+        }
+
+        private void OnHit()
+        {
+            gameObject.SetActive(false);
+        }
+
+
+        private IEnumerator AutoHide()
+        {
+            yield return new WaitForSeconds(5f);
+            gameObject.SetActive(false);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Chests/TrapBuilder.cs
@@ -0,0 +1,101 @@
+﻿namespace Treasure.Chests
+{
+    using UnityEngine;
+    using Treasure.Common;
+    using System.Collections.Generic;
+
+    public class TrapBuilder : MonoBehaviour
+    {
+        [SerializeField] private ContactFilter2D _contactFilter;
+        [SerializeField] private DamageInstigator _spikeTrap = null;
+        [SerializeField] private DataProperty[] _spikeTrapProperties;
+        [SerializeField] private ArrowTrap _arrowTrap = null;
+        private List<DamageInstigator> _spikeTraps = new List<DamageInstigator>();
+        private List<ArrowTrap> _arrowTraps = new List<ArrowTrap>();
+        private Vector2 _projectileMoveDirection;
+        private TrapType _trapType;
+        public void Init(int trapType)
+        {
+            _trapType = (TrapType)trapType;
+
+            switch (_trapType)
+            {
+                case TrapType.Spikes:
+                    BuildSpikesFromPrefab();
+                    break;
+                case TrapType.Arrow: 
+                    BuildArrowFromPrefab();
+                    break;
+                case TrapType.Fire: break;
+                default: break;
+            }
+        }
+
+
+
+        private void BuildArrowFromPrefab()
+        {
+            int randomArrowCount = Random.Range(1, 5);
+
+            for (int i = 0; i < randomArrowCount; i++)
+            {
+                ArrowTrap trap = Instantiate(_arrowTrap);
+                trap.Init();
+                _arrowTraps.Add(trap);
+            }
+        }
+
+        private void BuildSpikesFromPrefab()
+        {
+            _spikeTraps = new List<DamageInstigator>();
+
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    Vector2 squarePosition = new Vector2(x, y);
+                    RaycastHit2D[] results = new RaycastHit2D[8];
+                    int hits = Physics2D.Raycast((Vector2)transform.position + squarePosition, Vector2.zero, _contactFilter, results);
+
+                    if (hits < 1) //if there is a tile
+                    {
+                        DamageInstigator spike = Object.Instantiate(_spikeTrap, transform);
+                        spike.transform.position = (Vector2)transform.position + squarePosition;
+                        spike.Init(_spikeTrapProperties);
+                        _spikeTraps.Add(spike);
+                    }
+                    else
+                    {
+                        Debug.DrawRay((Vector2)transform.position + squarePosition, Vector2.down, Color.blue, 1000);
+                        Debug.Log(results[0].transform.name + " : " + squarePosition.ToString());
+                    }
+                }
+            }
+
+            foreach (var trap in _spikeTraps)
+                trap.ToggleInstigator(false);
+        }
+
+        public void ActivateGeneratedTraps()
+        {
+            foreach (var trap in _spikeTraps)
+            {
+                trap.ToggleInstigator(true);
+                trap.transform.GetChild(0).gameObject.SetActive(false);
+                trap.transform.GetChild(1).gameObject.SetActive(true);
+            }
+
+            foreach (var trap in _arrowTraps)
+                trap.Activate();
+        }
+    }
+
+    public enum TrapType
+    {
+        None,
+        Fire,
+        Arrow,
+        Spikes,
+    }
+}
+
